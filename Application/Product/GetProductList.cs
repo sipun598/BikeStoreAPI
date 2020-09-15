c411f34ec21719e8a90f070cb8679da301725acf
sipun598 <sipun598@gmail.com>
2020-09-15 20:40:48 +0530

    jenkin6

--- Application/Product/GetProductList.cs
+++ Application/Product/GetProductList.cs
@@ -56,7 +56,7 @@ namespace Application.Product
                 {
                     throw new RestException(HttpStatusCode.NotFound, new { product = "Not Found" });
                 }
-                //jenkin4
+                //jenkin5
                 var paginationObj = ReturnPaginationDto.GetPage(products, request.pagination.Page, request.pagination.RecordsPerPage, count);
                 var productsToReturn = mapper.Map<PaginationViewModel<Products>, ProductPaginationViewModel>(paginationObj);
                 return productsToReturn;
